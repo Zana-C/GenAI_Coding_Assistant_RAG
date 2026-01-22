8b220973aba85022b20128e624c2ce99a6b803d6
Zana <serhat.zana.ciftci@gmail.com>
2026-01-22 03:46:00 +0300

    Update import for HuggingFaceEmbeddings module

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -7,7 +7,7 @@ from typing import List, Dict
 # GEREKLİ KÜTÜPHANELER
 from datasets import load_dataset
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain_text_splitters import RecursiveCharacterTextSplitter
